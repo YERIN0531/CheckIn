a57db4605bd052b59a070a611780ecf853e4f456
Choijinyoung-9878 <chlwlsdud69@naver.com>
2022-08-01 11:30:16 +0900

    호텔 예약조회

---
+++ bin/src/main/resources/META-INF/db/checkin_customer.sql
@@ -0,0 +1,139 @@
+-------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
+---------------------------- CUSTOMER TABLE--------------------------------------------
+DROP TABLE CUSTOMER CASCADE CONSTRAINTS;
+CREATE TABLE CUSTOMER(
+    MID         VARCHAR2(50)  PRIMARY KEY,
+    MPW         VARCHAR2(50)  NOT NULL,
+    MNAME       VARCHAR2(50)  NOT NULL,
+    MENAME      VARCHAR2(50),
+    MPASSPORT   VARCHAR2(50),
+    MTEL        VARCHAR2(50)  NOT NULL,
+    MPOST       NUMBER(6)     NOT NULL,
+    MADDRESS    VARCHAR2(300) NOT NULL,
+    MDEADDRESS  VARCHAR2(200) NOT NULL,
+    MGENDER     VARCHAR2(10)  NOT NULL,
+    MBIRTH      DATE NOT NULL,
+    MWITHD      NUMBER(10) DEFAULT 1 NOT NULL,
+    MEMAIL      VARCHAR2(100) NOT NULL,
+    MRDATE      DATE DEFAULT SYSDATE,
+    MTOTAL      NUMBER(10) DEFAULT 0 NOT NULL,
+    MMILEAGE    NUMBER(10) DEFAULT 0 NOT NULL
+);
+SELECT * FROM CUSTOMER;
+INSERT INTO CUSTOMER VALUES ('aaa', '123', '서예린', 'SEOYERIN',NULL,'010-1111-1111','12345','서울 구로구','101동 101호','f','19950531',1,'tjqud531531@naver.com',SYSDATE,0,0);
+INSERT INTO CUSTOMER VALUES ('bbb', '123', '윤정임', 'YOONJEONGIM',NULL,'010-2222-2222','12346','서울 용산구','102동 102호','f','19950822',1,'zeolim@naver.com',SYSDATE,0,0);
+INSERT INTO CUSTOMER VALUES ('ccc', '123', '오동준', 'OHDONGJUN',NULL,'010-3333-3333','12347','서울 강남구','103동 103호','m','19951010',1,'dong@naver.com',SYSDATE,0,0);
+INSERT INTO CUSTOMER VALUES ('ddd', '123', '최진영', 'CHOIJINYOUNG',NULL,'010-4444-4444','12348','서울 서초구','104동 104호','m','19991203',1,'jin@naver.com',SYSDATE,0,0);
+INSERT INTO CUSTOMER VALUES ('eee', '123', '김빈', 'KIMBIN',NULL,'010-5555-6666','12349','서울 광진구','105동 105호','m','19960826',1,'bin@naver.com',SYSDATE,0,0);
+--1. 회원가입
+--joinMember
+INSERT INTO CUSTOMER VALUES 
+            ('fff', '123', '동구', 'DONG',NULL,'010-5555-6666','12349','서울 낙성대역','105동 105호','m','19960826',1,'dong@naver.com',SYSDATE,0,0);
+--2. 아이디 중복체크
+--idConfirm
+SELECT * FROM CUSTOMER WHERE MID='aaa';
+
+--2. 로그인
+--loginMember
+SELECT * FROM CUSTOMER WHERE MID='aaa' AND MPW='123';
+
+--3.회원상세보기(MID)
+--getDetailMember
+SELECT * FROM CUSTOMER WHERE MID ='aaa';
+
+--4.정보수정
+--modifyMember
+UPDATE CUSTOMER SET MPW='111',
+                    MNAME='서치코',
+                    MPASSPORT='111-111-11',
+                    MTEL='010-2312-1234',
+                    MPOST=3456,
+                    MADDRESS='독도',
+                    MDEADDRESS='1011동',
+                    MGENDER='m',
+                    MBIRTH='19990808',
+                    MWITHD=1,
+                    MEMAIL='ABC@NAVER.COM',
+                    MRDATE = SYSDATE,
+                    MTOTAL=0,
+                    MMILEAGE=0
+            WHERE MID ='aaa';
+                    
+--5.회원탈퇴 탈퇴여부 UPDATE (1 , 0)
+--withdrawMember
+UPDATE CUSTOMER SET mwithd = 0 WHERE MID ='eee';
+SELECT * FROM CUSTOMER WHERE MID='eee';
+
+--6.아이디찾기 
+--findldMember
+SELECT MID FROM CUSTOMER WHERE MID ='ddd';
+
+--7. 비밀번호찾기 (아이디 ,이메일 )
+--findPwMember
+SELECT MPW FROM CUSTOMER WHERE MID='aaa' AND MEMAIL='ABC@NAVER.COM';
+-------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
+---------------------------- CUSTOMER TABLE--------------------------------------------
+
+
+
+---------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
+--------------------------CUSTOMER_GRADE-----------------------------------------
+
+DROP TABLE CUSTOMER_GRADE;
+
+CREATE TABLE CUSTOMER_GRADE(
+    GRADE_NAME VARCHAR2(50) PRIMARY KEY,
+    LOWTOTAL NUMBER(10) NOT NULL,
+    HIGHTOTAL NUMBER(10) NOT NULL
+);
+
+SELECT * FROM CUSTOMER_GRADE;
+
+INSERT INTO CUSTOMER_GRADE VALUES ('브론즈',0,1000000);
+INSERT INTO CUSTOMER_GRADE VALUES ('실버',1000001, 5000000 );
+INSERT INTO CUSTOMER_GRADE VALUES ('골드',5000001,20000000);
+INSERT INTO CUSTOMER_GRADE VALUES ('플래티넘',20000001,50000000);
+INSERT INTO CUSTOMER_GRADE VALUES ('VIP', 50000001,9999999999);
+
+
+--------------------------CUSTOMER_GRADE-----------------------------------------
+---------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
+
+
+-------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
+--------------------------MANAGER------------------------------------------------
+DROP TABLE MANAGER;
+
+CREATE TABLE MANAGER(
+    AID VARCHAR2(20) PRIMARY KEY,
+    APW VARCHAR2(20) NOT NULL,
+    ANAME VARCHAR2(20) NOT NULL,
+    AEMAIL VARCHAR2(50) NOT NULL
+);
+
+INSERT INTO MANAGER VALUES ('admin','123','총관리자','chlwlsdud69@naver.com');
+INSERT INTO MANAGER VALUES ('subadmin','123','서브관리자','kok3443@naver.com');
+    --1.관리자 등록
+    -- joinAdmin
+    INSERT INTO MANAGER VALUES ('DELETE ','123','서브관리자','kok3443@naver.com');
+    --2.관리자 로그인
+    -- loginAdmin
+    SELECT * FROM MANAGER WHERE AID='admin' AND APW='123';
+    --3. 관리자 삭제 
+    -- deleteAdmin
+    SELECT * FROM MANAGER;
+    DELETE FROM MANAGER WHERE AID='DELETE ';
+    --4. 관리자탈퇴 
+    -- withdrawAdmin
+     DELETE FROM MANAGER WHERE AID='DELETE ';
+    
+
+SELECT * FROM MANAGER;
+--------------------------MANAGER------------------------------------------------
+---------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
---
+++ src/main/java/com/ocsy/checkin/service/HotelServiceImpl.java
@@ -0,0 +1,257 @@
+package com.ocsy.checkin.service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.ocsy.checkin.dao.HotelDao;
+import com.ocsy.checkin.dto.Hotel;
+import com.ocsy.checkin.dto.Hotel_ZZim;
+import com.ocsy.checkin.dto.Hotel_rs;
+
+@Service
+public class HotelServiceImpl implements HotelService {
+	@Autowired
+	private HotelDao hotelDao;
+	String backupPath = "C:\\TeamCheckin\\CheckIn\\src\\main\\webapp\\hotelImg/";
+	
+	@Override
+	public int insertHotel(MultipartHttpServletRequest mRequest,Hotel hotel) {
+		String uploadPath = mRequest.getRealPath("hotelImg/");
+		Iterator<String> params = mRequest.getFileNames();
+		String[] hotelimg = new String[4];
+		int idx = 0;
+		while(params.hasNext()) {
+			String param = params.next();
+			MultipartFile mFile = mRequest.getFile(param);
+			hotelimg[idx] = mFile.getOriginalFilename();
+			if(hotelimg[idx] != null && !hotelimg[idx].equals("")) {
+				if(new File(uploadPath + hotelimg[idx]).exists()) {
+					hotelimg[idx] = System.currentTimeMillis()+"_"+hotelimg[idx];
+				}
+				try {
+					mFile.transferTo(new File(uploadPath + hotelimg[idx]));
+					System.out.println("서버 파일 : " + uploadPath + hotelimg[idx]);
+					System.out.println("백업 파일 : " + backupPath);
+					boolean output = filecopy(uploadPath + hotelimg[idx], backupPath + hotelimg[idx]);
+					System.out.println(output ? idx+"번째 백업성공" : idx+" 번째 백업 실패");
+				} catch(IOException e) {
+					System.out.println(e.getMessage());
+				}
+			} else {
+				hotelimg[idx] = "";
+			}
+			idx++;
+		}
+		hotel.setHotelimage(hotelimg[0]);
+		hotel.setStandardimage(hotelimg[1]);
+		hotel.setDeluxeimage(hotelimg[2]);
+		hotel.setSweetimage(hotelimg[3]);
+		int result = 0;
+		result += hotelDao.insertHotel1(hotel);
+		result += hotelDao.insertHotel2(hotel);
+		result += hotelDao.insertHotel3(hotel);
+		if(result == 3) {
+			System.out.println("호텔 등록 성공");
+		}
+		return result;
+	}
+
+	@Override
+	public int deleteHotel(String hotelid) {
+		int result = 0;
+		result += hotelDao.deleteHZZim(hotelid);
+		result += hotelDao.deleteHotel(hotelid);
+		if(result >= 1) {
+			System.out.println("호텔 삭제 성공");
+		}
+		return result;
+	}
+
+	@Override
+	public int updateHotel(MultipartHttpServletRequest mRequest,Hotel hotel) {
+		String uploadPath = mRequest.getRealPath("hotelImg/");
+		Iterator<String> params = mRequest.getFileNames();  
+		String[] hotelimg = new String[1];  
+		int idx = 0;
+		while(params.hasNext()) {
+			String param = params.next();
+			MultipartFile mFile = mRequest.getFile(param); // 파라미터에 첨부된 파일 객체
+			hotelimg[idx] = mFile.getOriginalFilename();
+			if(hotelimg[idx] != null && !hotelimg[idx].equals("")) {  // 이럴경우 파일 첨부했을 경우
+				if(new File(uploadPath + hotelimg[idx]).exists()) {
+					hotelimg[idx] = System.currentTimeMillis()+"_"+hotelimg[idx];
+				}
+				try {
+					mFile.transferTo(new File(uploadPath + hotelimg[idx]));
+					System.out.println("서버 파일 : " + uploadPath + hotelimg[idx]);
+					System.out.println("백업 파일 : " + backupPath);
+					boolean result = filecopy(uploadPath + hotelimg[idx], backupPath + hotelimg[idx]);
+					System.out.println(result ? idx+"번째 백업성공" : idx+" 번째 백업 실패");
+				} catch (IOException e) {
+					System.out.println(e.getMessage());
+				}
+			} else { 
+				// 파일 첨부 안한경우 bimg[idx] = ""
+				hotelimg[idx] = "";
+			}
+			idx++;
+		}
+		hotel.setHotelimage(hotelimg[0]);
+		int result = hotelDao.updateHotel(hotel);
+		if(result == 3) {
+			System.out.println(hotel.getHotelname() + " 수정 성공");
+		}
+		return result;
+	}
+
+	@Override
+	public List<Hotel> getHotelDetail(String hotelid) {		
+		return hotelDao.getHotelDetail(hotelid);
+	}
+
+	@Override
+	public int updateHotelRoom(MultipartHttpServletRequest mRequest,Hotel hotel) {
+		String uploadPath = mRequest.getRealPath("hotelImg/");
+		Iterator<String> params = mRequest.getFileNames();  
+		String[] roomimage = new String[1];  
+		int idx = 0;
+		while(params.hasNext()) {
+			String param = params.next();
+			MultipartFile mFile = mRequest.getFile(param); // 파라미터에 첨부된 파일 객체
+			roomimage[idx] = mFile.getOriginalFilename();
+			if(roomimage[idx] != null && !roomimage[idx].equals("")) {  // 이럴경우 파일 첨부했을 경우
+				if(new File(uploadPath + roomimage[idx]).exists()) {
+					roomimage[idx] = System.currentTimeMillis()+"_"+roomimage[idx];
+				}
+				try {
+					mFile.transferTo(new File(uploadPath + roomimage[idx]));
+					System.out.println("서버 파일 : " + uploadPath + roomimage[idx]);
+					System.out.println("백업 파일 : " + backupPath);
+					boolean result = filecopy(uploadPath + roomimage[idx], backupPath + roomimage[idx]);
+					System.out.println(result ? idx+"번째 백업성공" : idx+" 번째 백업 실패");
+				} catch (IOException e) {
+					System.out.println(e.getMessage());
+				}
+			} else { 
+				// 파일 첨부 안한경우 bimg[idx] = ""
+				roomimage[idx] = "";
+			}
+			idx++;
+		}
+		hotel.setRoomimage(roomimage[0]);		
+		int result = hotelDao.updateHotelRoom(hotel);
+		return result;
+	}
+
+	@Override
+	public List<Hotel> checkHotelReservationList(Hotel hotel) {
+		return hotelDao.checkHotelReservationList(hotel);
+	}
+
+	@Override
+	public List<Hotel_rs> myHotelReservationList(String mid) {
+		return hotelDao.myHotelReservationList(mid);
+	}
+
+	@Override
+	public int canselHotel(int hrs_num) {
+		int result = hotelDao.canselHotel(hrs_num);
+		if(result == 1) {
+			System.out.println("호텔 예약 취소");
+		}
+		return result;
+	}
+
+	@Override
+	public int insertHotelReservation(Hotel_rs hotel_rs) {
+		int result = hotelDao.insertHotelReservation(hotel_rs);
+		if(result == 1) {
+			System.out.println("호텔 예약 성공");
+		}
+		return result;
+	}
+
+	@Override
+	public List<Hotel_rs> checkHotelReservation(Hotel hotel) {
+		return hotelDao.checkHotelReservation(hotel);  
+	}
+
+	@Override
+	public int insertHZZim(Hotel_ZZim hzzim) {
+		int result = hotelDao.insertHZZim(hzzim);
+		if(result == 1) {
+			System.out.println("호텔 찜 성공");
+		}
+		return result;
+	}
+
+	@Override
+	public int checkZZim(Hotel_ZZim hzzim) {
+		return hotelDao.checkZZim(hzzim);
+	}
+
+	@Override
+	public List<Hotel> myHZZimList(String mid) {
+		return hotelDao.myHZZimList(mid);
+	}
+
+	@Override
+	public int cancelHZZim(Hotel_ZZim hzzim) {
+		int result = hotelDao.cancelHZZim(hzzim);
+		if(result == 1) {
+			System.out.println("찜 취소 성공");
+		}
+		return result;
+	}
+	
+	private boolean filecopy(String serverFile, String backupFile) {
+		boolean isCopy = false;
+		// 복사
+		InputStream is = null;
+		OutputStream os = null;
+		try {
+			File file = new File(serverFile);
+			is = new FileInputStream(file);
+			os = new FileOutputStream(backupFile);
+			byte[] buff = new byte[(int) file.length()];  // 파일 크기를 1MB로 제한했기때문에 long 형이 들어올수 없으므로 int로 형변환
+			while(true) {
+				int nReadByte = is.read(buff);
+				if(nReadByte == -1) {
+					break;
+				}
+				os.write(buff, 0, nReadByte);
+			}
+			isCopy = true;
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage() + serverFile + " 이란 파일이 없습니다.");
+		} catch (IOException e) {
+			// 이거는 파일 읽어올때 예외 
+			System.out.println(e.getMessage());
+		} finally {
+			try {
+				if(os != null) {
+					os.close();
+				}
+				if(is != null) {
+					is.close();
+				}
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		return isCopy;
+	}
+
+}
