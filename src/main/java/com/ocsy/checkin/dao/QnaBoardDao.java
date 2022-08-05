98a459300238667cbae318855eae1c37098c55e1
OHDongJoon <kok3443@naver.com>
2022-08-05 09:52:26 +0900

    문의사항게시판 글쓰기 글삭제 답글작성

--- src/main/java/com/ocsy/checkin/dao/QnaBoardDao.java
+++ src/main/java/com/ocsy/checkin/dao/QnaBoardDao.java
@@ -10,15 +10,14 @@ import com.ocsy.checkin.dto.QnaBoard;
 public interface QnaBoardDao {
 	public List<QnaBoard> listQna(QnaBoard qna); // 글목록
 	public int countQna();						 // 글개수
-	public QnaBoard detailQna(QnaBoard qna);		 // 본인글 상세보기
-	//원래 public int detailQna(int qnum)
+	public int adminreply(QnaBoard qna);						//같은 group 개수 
+	public QnaBoard detailQna(QnaBoard qna);		 // 본인글 상세보기 , int 비번  , int qnum
+	public QnaBoard detailSecreat(QnaBoard qna);
 	public QnaBoard modifyreplyView(int qnum);   // 관리자글 수정 , 답변 view
 	public int insertQna(QnaBoard qna);			// 문의하기 원글
-	//public int hitupQna(int qnum);				// 히트업
-	public int hitupQna(QnaBoard qna);
+	public int hitupQna(int qnum);				// int qnum 만 했는데 이번에는 다름 
 	public int replyQna(QnaBoard qna);			// 관리자 답글
 	public int modifyQna(QnaBoard qna);			// 글수정
 	public int deleteQna(QnaBoard qna);		// 글삭제 답글까지 
-	public int adminreply();					// 답변글 여부 확인
 	public QnaBoard secreatView(int qnum);
 }
